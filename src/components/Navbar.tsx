2a59e1ef548af258f5ae71dd8e362f7153ee5b15
abhidatta0 <abhidatta146@gmail.com>
2026-08-31 20:48:08 +0530

    enhanced performance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     <Container className="md:p-0">
       <nav role="navigation" className="flex flex-row gap-3 items-center">
         <img
-          src="/my-photo.webp"
-          height={"100"}
-          width={"100"}
-          alt="avatar"
+          src="/avatar-180.webp"
+          width={60}
+          height={60}
+          alt="Abhirup Datta"
           className="size-15 rounded-full object-cover"
         />
         <h1 className="text-3xl md:text-5xl font-bold bg-linear-to-r from-cyan-600 to-blue-600 bg-clip-text text-transparent tracking-tight">
